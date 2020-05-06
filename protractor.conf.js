56f6276150fc5870cb1e99209296b57f54c10844
saddhamtajuddhin.pathan <saddhamtajuddhin.pathan@servicenow.com>
2020-05-06 12:29:11 +0530

    Conf changes

--- protractor.conf.js
+++ protractor.conf.js
@@ -56,7 +56,7 @@ exports.config = {
         'directConnect': true,
         'browserName': 'chrome',
         chromeOptions: {
-            args: ['--headless', '--disable-gpu', '--window-size=800x600']
+            args: ['--headless', '--no-sandbox', '--disable-dev-shm-usage']
         }
     },
 
